789938814008a773e303f4e2dc9f8c6ddad1d856
Nandini <nandinigujarathi@gmail.com>
2022-05-14 22:19:44 +0530

    Task component will render in only todo section

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
 
 @Component({
   selector: 'app-body',
@@ -6,7 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
- 
+
+
   constructor() { }
 
   ngOnInit(): void {
